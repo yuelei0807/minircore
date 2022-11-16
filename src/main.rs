fb47602f26f873f0b6053f8726c77f3ae18bea4d
yuelei0807 <44107854+yuelei0807@users.noreply.github.com>
2022-11-16 00:02:28 -0500

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -6,6 +7,22 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-fn main() {
-    
+
+static HELLO: &[u8] = b"This is a minircore.";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! { 
+    let vga_buffer = 0xb8000 as *mut u8;
+    //0xb8000 is the address of the VGA text buffer
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            //call the offset method to write the string byte
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            //0xb is the corresponding color byte
+        }
+    }
+    loop {}
 }
+
