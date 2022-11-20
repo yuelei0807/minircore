96efeb01793917ce891426ce3c2817de8bee5b87
yuelei0807 <yuelei0807@outlook.com>
2022-11-19 19:06:29 -0800

    overwrite C run time 0

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    loop {}
+}
