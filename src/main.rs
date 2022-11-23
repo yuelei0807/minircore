ebe6507f6d802029a8a4e927381b0029aa90a012
yuelei0807 <yuelei0807@outlook.com>
2022-11-23 09:28:57 -0800

    translated virtual addresses to physical addresses

--- src/main.rs
+++ src/main.rs
@@ -27,33 +27,48 @@ fn panic(info: &PanicInfo) -> ! {
 //define the real lower level _start entry point: kernel_main Rust function, using macro
 entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use minircore::memory::active_level_4_table;
+    //use minircore::memory::active_level_4_table;
+    use minircore::memory::translate_addr;
     use x86_64::VirtAddr;
-    use x86_64::structures::paging::PageTable;
     println!("This is a minircore!");
 
     minircore::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let level4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    let addresses = [
+        //the identity-mapped vga buffer page
+        0xb8000,
+        //some code page
+        0x201008,
+        //some stack page
+        0x0100_0020_1a10,
+        //virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
 
-    for (i, entry) in level4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("The entries of the level 4 table {}: {:?}", i, entry);
-            // get the physical address from the entry and convert it
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let level3_table: &PageTable = unsafe { &*ptr };
-
-            // print non-empty entries of the level 3 table
-            for (i, entry) in level3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("The entries of the level 3 table {}: {:?}", i, entry);
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
+    //let level4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    //for (i, entry) in level4_table.iter().enumerate() {
+    //    if !entry.is_unused() {
+    //        println!("The entries of the level 4 table {}: {:?}", i, entry);
+    //        // get the physical address from the entry and convert it
+    //        let phys = entry.frame().unwrap().start_address();
+    //        let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //        let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //        let level3_table: &PageTable = unsafe { &*ptr };
+    //
+            // print non-empty entries of the level 3 table
+            //for (i, entry) in level3_table.iter().enumerate() {
+                //if !entry.is_unused() {
+                    //println!("The entries of the level 3 table {}: {:?}", i, entry);
+                //}
+            //}
+        //}
+    //}
     #[cfg(test)]
     test_main();
     println!("It did not crash!");
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,7 @@
 use x86_64::{
     structures::paging::PageTable,
     VirtAddr,
+    PhysAddr,
 };
 
 //the complete physical memory is mapped to virtual memory at  the passed 'physical_memory_offset`. Also, this function must be called once
@@ -19,4 +20,43 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr // unsafely return &mut PageTable reference
+}
+
+//Translates the given virtual address to the mapped physical address, or `None` if the address is not mapped
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr)
+    -> Option<PhysAddr>
+{
+    use x86_64::structures::paging::page_table::FrameError;
+    use x86_64::registers::control::Cr3;
+
+    // read the active level 4 frame from the CR3 register
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
+    ];
+    let mut frame = level_4_table_frame;
+
+    // traverse the multi-level page table
+    for &index in &table_indexes {
+        // convert the frame into a page table reference
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe {&*table_ptr};
+
+        // read the page table entry and update `frame`
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        };
+    }
+
+    // calculate the physical address by adding the page offset
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
